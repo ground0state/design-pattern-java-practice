11404a0038ad84e2b352c99448f4a3b9c53628f9
masafumi <masafumi.abeta@gmail.com>
2019-12-21 00:36:21 +0900

    Add files

---
+++ src/Visitor/sample/Element.java
@@ -0,0 +1,5 @@
+package Visitor.sample;
+
+public interface Element {
+  public abstract void accept(Visitor v);
+}
---
+++ src/Visitor/sample/FileTreatmentException.java
@@ -0,0 +1,12 @@
+package Visitor.sample;
+
+public class FileTreatmentException extends RuntimeException {
+  public FileTreatmentException() {
+
+  }
+
+  public FileTreatmentException(String msg) {
+    super(msg);
+  }
+
+}
